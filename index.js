ac82574cbdf9a43a9f5636e4388ee37ecb0291d4
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-02-05 20:29:52 +0100

    committed

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const vans = [
     { id: 6, name: "Green Wonder", price: 70, description: "With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/green-wonder.png", type: "rugged", hostId: "123" }
 ];
 
+
 app.get('/api/vans', (req, res) => {
     res.send(vans);
 });
